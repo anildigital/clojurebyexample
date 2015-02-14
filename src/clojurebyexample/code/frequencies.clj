c4f8092f64dc05199c15e20ed778144cdef49ff5
Akshat Jain <akshatj42@gmail.com>
2015-02-14 19:55:58 +0530

    Added frequencies

---
+++ src/clojurebyexample/code/frequencies.clj
@@ -0,0 +1,10 @@
+;; frequencies
+(ns frequencies)
+
+;; Takes a list or vector and returns the frequency of elements in it
+
+(frequencies [:woof :meow :woof :woof]) 
+
+;; Returns
+{:woof 3, :meow 1} 
+
