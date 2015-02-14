9d2b3b232d32e2765a9bf1350e9b69868e2c5146
Akshat Jain <akshatj42@gmail.com>
2015-02-14 20:11:42 +0530

    Made a small correction to frequencies

--- src/clojurebyexample/code/frequencies.clj
+++ src/clojurebyexample/code/frequencies.clj
@@ -1,7 +1,7 @@
 ;; frequencies
 (ns frequencies)
 
-;; Takes a list or vector and returns the frequency of elements in it
+;; Takes a vector and returns the frequency of elements in it
 
 (frequencies [:woof :meow :woof :woof]) 
 
