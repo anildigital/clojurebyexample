d79f44efa7248da0a96ff12f26b5349cce4ac27d
Anil Wadghule <anildigital@gmail.com>
2015-02-17 21:21:33 +0530

    Fix heroku deployment

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-marginalia "0.8.0"] [lein-ring "0.9.1"] [compojure "1.3.1"]]
   :resource-paths ["resources"] ;
+  :min-lein-version "2.0.0"
   :ring {:handler clojurebyexample.core/app  }
   :dependencies [[org.clojure/clojure "1.6.0"]])
